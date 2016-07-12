d6a0fde74f940954260ca50cd8d11c2d2044ace9
Shohei Miyashita <shohei.miyashita.212@gmail.com>
2016-07-12 15:12:37 +0900

    fix

--- luxembourg/dqn/action_value.py
+++ luxembourg/dqn/action_value.py
@@ -19,14 +19,19 @@ class ActionValue(Chain):
             l2=F.Linear(1000, 1000),
             l3=F.Linear(1000, 1000),
             l4=F.Linear(1000, 1000),
-            q_value=F.Linear(1000, n_act))
+            l5=F.Linear(1000, n_act))
 
-    def q_function(self, state):
+    def action_probability(self, state):
         """
-        Q-function
+        The param `state` is the collection.
+        It has the value which indicate whether or not corresponding point is selected
+        Indicator values:
+            0 : the point is empty
+            1 : the point has been already selected
+        :state: 15-length array (In the case of 5-height game)
         """
         h1 = F.relu(self.l1(state))
         h2 = F.relu(self.l2(h1))
         h3 = F.relu(self.l3(h2))
         h4 = F.relu(self.l4(h3))
-        return self.q_value(h4)
+        return self.l2(h4)
